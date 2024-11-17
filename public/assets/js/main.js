1500fa8f3fdd1e7673c6542a4715619a4b067662
jcazul <jcarl.azul@gmail.com>
2024-11-17 17:19:46 -0500

    Updated gitignore, added icons, and modified some styles

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,9 +2,9 @@ var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+                labels: ['0', '1', '2', '3', '4', '5', '6','7', '8', '9', '10', '11', '12', '13','14', '15', '16', '17', '18', '19', '20','21','22'],
                 datasets: [{
-                    data: [23, 14, 45, 41, 14, 67, 44],
+                    data: [23, 14, 45, 41, 14, 67, 44, 19, 30, 45, 52, 60, 20, 18, 34, 45, 35, 50, 39, 12, 46, 25],
                     lineTension: 0,
                     backgroundColor: 'transparent',
                     borderColor: '#007bff',
@@ -24,4 +24,4 @@ var ctx = document.getElementById('myChart').getContext('2d');
                     display: false
                 }
             }
-        });
+        })
